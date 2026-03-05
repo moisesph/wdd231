ae6c0c4cf2aa9e9912a843b9302ebb3fa1dc450a
moisesph <mpleytez@byupathway.edu>
2026-03-05 09:46:24 -0600

    adffda

---
+++ scripts/navigation.js
@@ -0,0 +1,13 @@
+const navbuttom = document.querySelector('#ham-btn');
+
+navbuttom.addEventListener('click', () => {
+    navbuttom.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
+
+const navBar = document.querySelector('#nav-bar');
+
+
+
+
