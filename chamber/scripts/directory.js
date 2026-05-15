03fa96735038b48e1a318cf32b58b98833a6dd72
moisesph <mpleytez@byupathway.edu>
2026-05-15 06:30:48 -0600

    adfdfa

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,8 +4,8 @@ const pcWidth = window.matchMedia('(min-width:a 38rem').matches;
 
 const companies = 'https://raw.githubusercontent.com/moisesph/wdd231/refs/heads/main/chamber/data/members.json';
 const cards = document.querySelector('#cards')
-const companiesRandom = companies.sort(() => Math.random() - 0.5)
-const companiesLimited = companiesRandom.slice(0, 3);
+//const companiesRandom = companies.sort(() => Math.random() - 0.5)
+//const companiesLimited = companiesRandom.slice(0, 3);
 
 async function getCompanyInformation() {
     const response = await fetch(companies);
