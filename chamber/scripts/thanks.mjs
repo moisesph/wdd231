495c04378dd44e35119b50a34fed83b0adb24168
moisesph <mpleytez@byupathway.edu>
2026-05-31 20:31:16 -0600

    adfsfdg

--- chamber/scripts/thanks.mjs
+++ chamber/scripts/thanks.mjs
@@ -1,8 +1,11 @@
 const myInfo = new URLSearchParams(window.location.search);
 
 document.querySelector('#thanks').innerHTML = `
-<p>Appointment for ${myInfo.get('email')} ${myInfo.get('last')}</p>
-<p>Proxy ${myInfo.get('ordinance')} on ${myInfo.get('date')} in the ${myInfo.get('location')} Temple</p>
-<p><p>Your phone: ${myInfo.get('phone')} </p>
-<p>Your email is ${myInfo.get('email')}</p>
+<p>Thank you for join ${myInfo.get('fname')} ${myInfo.get('lname')}</p>
+<p>We saved your email ${myInfo.get('email')}</p>
+<p>And your phone number ${myInfo.get('phone')} </p>
+<p>${myInfo.get('organization')} has successfully joined</p>
+<p>Your plan is: ${myInfo.get('membership')}</p>
+<br>
+<p>We hope we see you soon AWA</p>
 `;
