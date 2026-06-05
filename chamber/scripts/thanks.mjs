074114063654e5cebaeee11b827e7926b56e85d0
moisesph <mpleytez@byupathway.edu>
2026-06-04 18:12:46 -0600

    fadadf

--- chamber/scripts/thanks.mjs
+++ chamber/scripts/thanks.mjs
@@ -1,11 +1,13 @@
 const myInfo = new URLSearchParams(window.location.search);
 
 document.querySelector('#thanks').innerHTML = `
-<p>Thank you for join ${myInfo.get('fname')} ${myInfo.get('lname')}</p>
-<p>We saved your email ${myInfo.get('email')}</p>
-<p>And your phone number ${myInfo.get('phone')} </p>
-<p>${myInfo.get('organization')} has successfully joined</p>
-<p>Your plan is: ${myInfo.get('membership')}</p>
+<p>Thank you for join <font color="#155650">${myInfo.get('fname')} ${myInfo.get('lname')}</font></p>
+<p>We saved your email <font color="#1f7396">${myInfo.get('email')}</font></p>
+<p>And your phone number <font color="#1f7396">${myInfo.get('phone')}</font> </p>
+<p><font color="#1f7396">${myInfo.get('organization')}</font> has successfully joined</p>
+<p>Your plan is: <font color="#1f7396">${myInfo.get('membership')}</font></p>
 <br>
-<p>We hope we see you soon AWA</p>
+<p>We hope we see you soon AWA!!</p>
+
+<img src="images/thanks-image.jfif" alt="thanks image" id="thanks-image">
 `;
