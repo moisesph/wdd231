ad9dd34d8e609ebb2a42db7681eacf27b4d160ae
moisesph <mpleytez@byupathway.edu>
2026-06-14 06:51:36 -0600

    adfafd

--- chamber/scripts/discover-cards.mjs
+++ chamber/scripts/discover-cards.mjs
@@ -5,6 +5,7 @@ const cards = document.querySelector('#discover-cards');
 async function getPlaces() {
     try {
         console.log(discoverInfo);
+        showPlaces(discoverInfo);
     }
     catch (error) {
         console.error("Couldn't get data Json", error);
@@ -24,9 +25,27 @@ async function showPlaces(places) {
         let btn = document.createElement('button');
 
         title.textContent = `${place.name}`;
+        figureTxt.textContent = `${place.name}`;
+        address.textContent = `${place.location}`;
+        description.textContent = `${place.description}`;
+        btn.textContent = 'Visit';
+        image.setAttribute('src', place.image);
+        image.setAttribute('alt', `image of ${place.name}`);
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '10');
+        image.setAttribute('height', '10');
+
+        card.appendChild(title);
+        card.appendChild(figure);
+        figure.appendChild(image);
+        figure.appendChild(figureTxt);
+        card.appendChild(address);
+        card.appendChild(description);
+        card.appendChild(btn);
+
+        cards.appendChild(card);
 
     }
 }
 
 getPlaces();
-showPlaces(discoverInfo);
