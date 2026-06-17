a01655001f936f9b5eadbddd6f69b9b27aad04bc
moisesph <mpleytez@byupathway.edu>
2026-06-17 06:41:17 -0600

    dfsahgd

--- chamber/data/interest-items.mjs
+++ chamber/data/interest-items.mjs
@@ -4,55 +4,55 @@ export default [
         "location": "100 Maya Ruins Blvd, Copán Ruinas 41101",
         "url": "https://en.wikipedia.org/wiki/Cop%C3%A1n",
         "description": "An ancient Maya city famous for its detailed stone stelae, impressive hieroglyphic stairway, and historic ball court.",
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAF32jj1vy8Rdnv72ZsUYwIal5yVdaVtu3NT6gZAxsmYeWCJdCiVU5BvOnOtrHs1qwkiLcQWe8ywfL7lp2eiIojQO3d3NNiAaiyKC5hrVR3ZcOhcRJAoTDFxqeLeTRnwKMzSqem9=w1080-h624-n-k-no"
+        "image": "../images/1.webp"
     },
     {
         "name": "Roatán",
         "location": "250 West End Rd, Roatán 34101, Honduras",
         "url": "https://en.wikipedia.org/wiki/Roat%C3%A1n",
         "description": "A stunning Caribbean island known for its white-sand beaches, vibrant nightlife, and the world's second-largest coral reef.",
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAFfPtwyo9lpyA9FY1OQSkmTqa-pCimwsOmBwj7s3bzBCeFW9cGM7kySE_Fvm1fkwxfPL8QODA0llUqU7NYylUDjAKNEWMddY6hIelAwMU7mkf1JVSN2-dx_ZrTrvDavEga9KH4P2Q=w1080-h624-n-k-no"
+        "image": "../images/2.webp"
     },
     {
         "name": "Parque Nacional La Tigra",
         "location": "Km 22 Jutiapa Trail, La Tigra National Park 11101, Honduras",
         "url": "https://en.wikipedia.org/wiki/La_Tigra_National_Park",
         "description": "A lush, high-altitude cloud forest offering scenic hiking trails, dense vegetation, and vital wildlife habitats.",
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAFUBjpOYEuv7ZDqjTcgZF7AibG5RgwIFLGXEVJjG6h7WHXc-tP-OnZyRmIfbvtp_gfFtXrYlrvP_RdBClBKgYESPt5cWIplme6eO8vtjjrEt7-bqPvPPJtkDbPRfvT3MahN65GC1w=w270-h312-n-k-no"
+        "image": "../images/3.webp"
     },
     {
         "name": "Cayos Cochinos",
         "location": "12 Marine Reserve Way, Cayos Cochinos 34101, Honduras",
         "url": "https://en.wikipedia.org/wiki/Cayos_Cochinos",
         "description": "A pristine group of protected islands with crystal-clear waters, marine biodiversity, and a peaceful, untouched atmosphere.",
-        "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRACZqKjFozFEUCXWQXkiCh5xkhf126BewsEuXEQ7ecuYsb_2gmwo-TpGxZoY7pmkgueFt_wFThcEenLLc5kypFaTk&s=19"
+        "image": "../images/4.webp"
     },
     {
         "name": "Lago de Yojoa",
         "location": "Km 160 CA-5 Highway, Lake Yojoa 21101, Honduras",
         "url": "https://en.wikipedia.org/wiki/Lake_Yojoa",
         "description": "The country's only volcanic lake, surrounded by misty mountains, diverse bird species, and local lakeside restaurants.",
-        "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRq8LcZFODUKukU43ARSEBabqDl90EptNzpyGzVebVgASdTup3p7sAwSDwaDbbDds3JgWgtw3Ev-Hr-oJz4ajZgNPw&s=19"
+        "image": "../images/5.webp"
     },
     {
         "name": "Cataratas de Pulhapanzak",
         "location": "45 Waterfall Hwy, San Buenaventura 21102, Honduras",
         "url": "https://en.wikipedia.org/wiki/Pulhapanzak",
         "description": "A spectacular 43-meter waterfall featuring hidden caves, zip-lining, and beautiful natural swimming pools.",
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAFQ6LZwqeUUm7QmCUww66zVtzGIcl8dE4QMaVt4ctqZX6Cj8yG82LwKDkOBu7JHfnIr6Q2TmmN9jBSeA5aMvQl87jL7yTk6qHvQU6CixlKGsJKFjR63acN31tUI-uAw4uJ9nT5e=w302-h312-n-k-no"
+        "image": "../images/6.webp"
     },
     {
         "name": "Parque Nacional Pico Bonito",
         "location": "800 Cangrejal River Rd, Pico Bonito National Park 31101, Honduras",
         "url": "https://en.wikipedia.org/wiki/Pico_Bonito_National_Park",
         "description": "A towering coastal mountain range popular for rainforest trekking, wildlife watching, and whitewater rafting on the Cangrejal River.",
-        "image": "https://upload.wikimedia.org/wikipedia/commons/6/66/Pico_Bonito.JPG"
+        "image": "../images/7.webp"
     },
     {
         "name": "Comayagua",
         "location": "15 Central Plaza, Comayagua 12101",
         "url": "https://en.wikipedia.org/wiki/Comayagua",
         "description": "A charming colonial city rich in history, featuring Spanish architecture and the oldest functioning clock in the Americas.",
-        "image": "https://lh3.googleusercontent.com/gps-cs-s/APNQkAHtD_266XJZmA_LDu2A4e8nnO-TCQk5_qfhoAnOeWcU743CpUcLtp6du1livOYgoZkxaVXpPXaH9y77LJLLvSszM2JTeC9-xJNhzu6b2sHak5oD5s8kNFE5jNKFEXjH2C0XeAJeLA=w810-h468-n-k-no"
+        "image": "../images/8.webp"
     }
 ]
--- final-project/scripts/thanks.mjs
+++ final-project/scripts/thanks.mjs
@@ -7,9 +7,9 @@ document.querySelector('#thanks').innerHTML = `
 <p>We saved your email <font color="#1f7396">${myInfo.get('email')}</font></p>
 <p>And your phone number <font color="#1f7396">${myInfo.get('phone')}</font> </p>
 <p>You contacted us for : <font color="#1f7396">${myInfo.get('membership')}</font></p>
-<p>We will contact you as soon as possible!</p>
 <p>You asked an appointment on: <font color="#1f7396">${myInfo.get('date')}</font></p>
 <p>Hour: <font color="#1f7396">${myInfo.get('hour')}</font></p>
+<p>We will contact you as soon as possible!</p>
 <br>
 <p>We hope we see you soon <font color="#155650">${myInfo.get('fname')}</font></p> AWA!!</p>
 
