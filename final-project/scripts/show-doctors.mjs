b9ab776aa674fd5c52a6383189c15ab74f30c12a
moisesph <mpleytez@byupathway.edu>
2026-06-17 11:01:46 -0600

    dfafad

--- final-project/scripts/show-doctors.mjs
+++ final-project/scripts/show-doctors.mjs
@@ -5,7 +5,7 @@ const cards = document.querySelector('#doctors');
 async function getDoctors() {
     try {
         const response = await fetch(doctorsInfo);
-        const data = await response.json()
+        const data = await response.json(cards)
         showDoctors(data);
     }
     catch (error) {
